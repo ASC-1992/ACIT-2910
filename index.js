8b7f75bb0a9c8ff662508c4a26727e0e2fd3166b
Alex <alex_christensen92@hotmail.com>
2017-05-25 01:03:06 -0700

    Added 2 more contraints, having a max of 10 UNIQUE items and a max of 6 qty of a food item

--- index.js
+++ index.js
@@ -313,9 +313,42 @@ app.post("/menuCount", function(req, resp){
     });
 });
 
+//This is for the contraint of only adding unique items to the Cart
+app.post("/orderingIndividualItem", function(req, resp){
+    var itemName = req.body.itemName;
+     pg.connect(dbURL, function(err, client, done){
+        if(err){
+            console.log(err);
+            var obj = {
+                status: "fail",
+                msg: "CONNECTION FAIL"
+            }
+            resp.send(obj);
+        }
+            if(req.session.orderNum){
+                client.query("SELECT * FROM items WHERE orderid = ($1) AND itemname = ($2)", [req.session.orderNum, itemName], function(err, result){
+                   done();
+                    if(err){
+                        console.log(err);
+                    }
+
+                    if(result.rows.length > 0){
+                        resp.send({status:"fail"})
+                    } else {
+                        resp.send({status:"success"})
+                    }
+                });  
+            } else {
+                //Reason I am still sending a success is because since their session orderNum isnt defined and they will get it on the next ajax call... this is fundamentally broken as they can log out and then log in again to order the same item as they have already found
+                resp.send({status:"success"});
+            }
+     });
+});
+
 app.post("/ordering", function(req, resp){
     var orderName = req.body.itemName;
     var orderPrice = req.body.price;
+    var orderQty = req.body.qty
     var b00lean;
     
     if(req.session.itemCount > 9){
@@ -385,7 +418,7 @@ app.post("/ordering", function(req, resp){
 
     
     function insertItems(client, done, rr){
-        client.query("INSERT INTO items (orderid, itemname, itemqty, price) VALUES ($1, $2, $3, $4)", [rr, orderName, 1, orderPrice],function(err, result){
+        client.query("INSERT INTO items (orderid, itemname, itemqty, price) VALUES ($1, $2, $3, $4)", [rr, orderName, orderQty, orderPrice],function(err, result){
             done();
             if(err){
                 console.log(err);
